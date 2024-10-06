de3dba4d184279dd658d2baca4b3a14ab4a60d6a
accaliskan1064 <117007617+accaliskan1064@users.noreply.github.com>
2024-10-06 10:43:45 +0300

    committed

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-import App from "./App.jsx";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
